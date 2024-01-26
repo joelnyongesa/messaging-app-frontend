20700348f6ddecc3256b313ff1349053b7bb0781
joelnyongesa <joelnyongesa148@gmail.com>
2024-01-26 20:44:36 +0300

    feat: added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "../img/branch logo.png"
+
+function Navbar() {
+  return (
+    <div className='flex justify-center'>
+        <img src={logo} alt='Branch Logo' className=''/>
+    </div>
+  )
+}
+
+export default Navbar
